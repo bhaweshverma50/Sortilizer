ef82d669c2e0b3715db17c1ca8f93aaa432b165b
BhaweshVerma <bhaweshverma50@gmail.com>
2020-04-17 20:29:11 +0530

    added bars

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ var Barslider = document.getElementById("myRange");
 var Speedslider = document.getElementById("speed");
 var op = document.getElementById("value");
 var Sop = document.getElementById("Svalue");
+let n = 4;
 
 op.innerHTML = Barslider.value;
 Sop.innerHTML = -(Speedslider.value) + "ms";
 
-Barslider.oninput = function () {
+Barslider.oninput = function barval() {
     op.innerHTML = this.value;
-
 }
 
 Speedslider.oninput = function () {
     Sop.innerHTML = -(this.value) + "ms";
-
 }
 
 Barslider.addEventListener("mousemove", function () {
@@ -24,10 +23,11 @@ Barslider.addEventListener("mousemove", function () {
 })
 
 Speedslider.addEventListener("mousemove", function () {
-    var x = Speedslider.value;
-    x = x / 2;
-    var color = 'linear-gradient(90deg, rgb(117, 252, 117)' + x + '%, rgb(214, 214, 214)' + x + '%)';
+    var y = -(Speedslider.value);
+    y = y / 100;
+    var color = 'linear-gradient(90deg, rgb(117, 252, 117)' + y + '%, rgb(214, 214, 214)' + y + '%)';
     Speedslider.style.background = color;
+    console.log(Speedslider.value);
 })
 
 $(document).ready(function () {
@@ -36,6 +36,23 @@ $(document).ready(function () {
         console.log(txt);
     })
 })
+
+let div;
+let a = [];
+let bar = document.getElementById("action");
+
+bargen(n);
+
+function bargen(n) {
+    for (i = 0; i < n; i++) {
+        div = document.createElement('div');
+        div.className = 'bar';
+        div.style.height = 30 * 2 + 'px';
+        div.style.width = 50 + 'px';
+        div.id = i;
+        bar.appendChild(div);
+    }
+}
 
 function bubS() {
     console.log("true");
