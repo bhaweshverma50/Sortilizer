a47ecd9d1f63f38113bac164f2eb67fa04c53ff2
BhaweshVerma <bhaweshverma50@gmail.com>
2020-04-17 19:30:16 +0530

    new responsive web design(new beginning)

--- app.js
+++ app.js
@@ -1,152 +1,42 @@
-let numb = document.getElementById("num");
-let rand = document.getElementsByClassName("rnum");
-let a = [];
-let div;
-let n = numb.innerText;
-let r = rand.innerText;
-arrGen(n);   //initial array generation
-barGen(n);   //initial bar generation
+var Barslider = document.getElementById("myRange");
+var Speedslider = document.getElementById("speed");
+var op = document.getElementById("value");
+var Sop = document.getElementById("Svalue");
 
+op.innerHTML = Barslider.value;
+Sop.innerHTML = -(Speedslider.value) + "ms";
 
-
-function barGen(k, b1, b2) {
-    let barWidth = 1;
-    if (k <= 17) {
-        barWidth = 80;
-    } else if (k > 17 && k <= 27) {
-        barWidth = 50;
-    } else if (k > 27 && k <= 42) {
-        barWidth = 30;
-    } else if (k > 42 && k <= 60) {
-        barWidth = 20;
-    } else if (k > 60 && k <= 100) {
-        barWidth = 10;
-    } else if (k > 100 && k <= 150) {
-        barWidth = 5;
-    } else if (k > 150 && k <= 250) {
-        barWidth = 2;
-    }
-    for (i = 0; i < k; i++) {
-        div = document.createElement('div');
-        div.className = "bar";
-        div.style.height = a[i] * 2 + 'px';
-        div.style.width = barWidth + 'px';
-        div.id = a[i];
-        // while (barWidth >= 20)
-        if (barWidth >= 20)
-            div.innerHTML = "<span class='rnum'>" + a[i] + "</span>";
-        let bar = document.getElementById("action");
-        //coloring b1 and b2
-        if (i == b1) {
-            div.style.backgroundColor = "green";
-        }
-        if (i == b2) {
-            div.style.backgroundColor = "blue";
-        }
-        bar.appendChild(div);
-        console.log(div);
-        console.log(b1, b2);
-    }
-    console.log(a);
-}
-
-function arrGen(k) {
-    for (i = 0; i < k; i++) {
-        //r = Math.round(Math.random() * 177) + Math.round(Math.random() * 17);
-        r = Math.round(Math.random() * 100 * 2) + 2;
-        a.push(r);
-    }
-}
-
-//deleting array
-function delArr() {
-    a = [];
-}
-//deleting bars
-function barDel() {
-    let delB = document.getElementById("action");
-    while (delB.firstChild)
-        delB.removeChild(delB.firstChild);
-}
-
-//counter
-function incdec(i) {
-    console.log(r);
-    if (i == 1 && n < 220) {
-        n++;
-        clearInterval(x);
-        delArr();
-        arrGen(n);
-        barDel();
-        barGen(n);
-    } else if (i == -1 && n == 4) {
-        n = 220;
-        clearInterval(x);
-        delArr();
-        arrGen(n);
-        barDel();
-        barGen(n);
-    } else if (i == -1) {
-        n--;
-        clearInterval(x);
-        delArr();
-        arrGen(n);
-        barDel();
-        barGen(n);
-    }
-    numb.innerText = n;
-    rand.innerText = r;
-}
-
-//clear or reset
-function cl(j) {
-    if (j == 0) {
-        n = 4;
-        numb.innerText = n;
-        delArr();
-        arrGen(n);
-        barDel();
-        barGen(4);
-        clearInterval(x);
-        console.log(n);
-    }
+Barslider.oninput = function () {
+    op.innerHTML = this.value;
 
 }
 
-//bubble sort and speed control
+Speedslider.oninput = function () {
+    Sop.innerHTML = -(this.value) + "ms";
+
+}
+
+Barslider.addEventListener("mousemove", function () {
+    var x = Barslider.value;
+    x = x / 2;
+    var color = 'linear-gradient(90deg, rgb(117, 252, 117)' + x + '%, rgb(214, 214, 214)' + x + '%)';
+    Barslider.style.background = color;
+})
+
+Speedslider.addEventListener("mousemove", function () {
+    var x = Speedslider.value;
+    x = x / 2;
+    var color = 'linear-gradient(90deg, rgb(117, 252, 117)' + x + '%, rgb(214, 214, 214)' + x + '%)';
+    Speedslider.style.background = color;
+})
+
+$(document).ready(function () {
+    $('#listel a').on('click', function () {
+        var txt = ($(this).text());
+        console.log(txt);
+    })
+})
+
 function bubS() {
-    let s = 10;
-    x = setInterval(function () {
-        console.log("sort");
-        bubbleSort(a);
-    }, s);
-    barDel();
-    barGen(n);
-    console.log(a);
-}
-
-//swaping 
-function swap(arr, first_Index, second_Index) {
-    var temp = arr[first_Index];
-    arr[first_Index] = arr[second_Index];
-    arr[second_Index] = temp;
-    barDel();
-    barGen(n, first_Index, second_Index);
-}
-
-//bubble sort algorithm
-function bubbleSort(arr) {
-
-    var len = arr.length,
-        i, j, stop;
-
-    for (i = 0; i < len; i++) {
-        for (j = 0, stop = len - i; j < stop; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-                return arr;
-            }
-        }
-    }
-    clearInterval(x);
+    console.log("true");
 }
